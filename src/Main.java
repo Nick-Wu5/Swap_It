1086da2c5bc71e160f8bd5ced3bc483b7eb4d9bb
Nick Wu <wu2264@purdue.edu>
2024-11-03 23:22:05 -0500

    Updated main class with testing for user auth and posts

--- src/Main.java
+++ src/Main.java
@@ -53,5 +53,70 @@ public class Main {
         System.out.println(user1.getUsername() + " removed " + user2.getUsername() + " from friends list.");
         System.out.println(user1.getUsername() + "'s updated friends list: " + user1.getFriends());
 
+        // Creating and displaying a news post
+        System.out.println("\nCreating and Displaying News Posts:");
+        NewsPost post1 = new NewsPost(user1, "New Album Release", "/images/album.jpg", "2024-11-03");
+        System.out.println("Post Author: " + post1.getAuthor().getUsername());
+        System.out.println("Post Title: " + post1.getTitle());
+        System.out.println("Post Image Path: " + post1.getImagePath());
+        System.out.println("Post Date: " + post1.getDate());
+        System.out.println("Initial Upvotes: " + post1.getUpvotes());
+
+        // Upvoting and downvoting
+        post1.incrementUpvotes();
+        post1.incrementUpvotes();
+        post1.incrementDownvotes();
+        System.out.println("Upvotes after voting: " + post1.getUpvotes());
+
+        // Adding comments
+        NewsComment comment1 = new NewsComment("This is amazing!", user2);
+        NewsComment comment2 = new NewsComment("Can't wait to hear it!", user3);
+        post1.addComment(comment1);
+        post1.addComment(comment2);
+
+        // Upvoting comments
+        comment1.incrementUpvotes();
+        comment1.incrementUpvotes();
+        comment2.incrementUpvotes();
+
+        // Display comments
+        System.out.println("\nComments on the post:");
+        for (NewsComment comment : post1.getComments()) {
+            System.out.println("Comment Author: " + comment.getAuthor().getUsername());
+            System.out.println("Comment Content: " + comment.getContent());
+            System.out.println("Comment Upvotes: " + comment.getUpvotes());
+        }
+
+        // Demonstrating secure account creation
+        System.out.println("\nCreating New User Accounts:");
+        CreateNewUser newUser1 = new CreateNewUser("johnsmith", "securePass123");
+        CreateNewUser newUser2 = new CreateNewUser("janedoe", "password456");
+
+        // Displaying user registration status
+        if (!newUser1.isAlreadyRegistered()) {
+            System.out.println("User 'johnsmith' registered successfully!");
+        } else {
+            System.out.println("User 'johnsmith' is already registered.");
+        }
+        if (!newUser2.isAlreadyRegistered()) {
+            System.out.println("User 'janedoe' registered successfully!");
+        } else {
+            System.out.println("User 'janedoe' is already registered.");
+        }
+
+        // Demonstrating password-protected login
+        System.out.println("\nPassword-Protected Login:");
+        PasswordProtectedLogin loginSystem = new PasswordProtectedLogin();
+        if (loginSystem.authenticate("johnsmith", "securePass123")) {
+            System.out.println("Login successful for 'johnsmith'.");
+        } else {
+            System.out.println("Login failed for 'johnsmith'.");
+        }
+
+        if (loginSystem.authenticate("janedoe", "wrongPassword")) {
+            System.out.println("Login successful for 'janedoe'.");
+        } else {
+            System.out.println("Login failed for 'janedoe'.");
+        }
     }
 }
