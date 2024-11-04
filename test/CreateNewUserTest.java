e9f4265e1d460ab17e8de97bf93ba713948c7ac6
Nick Wu <wu2264@purdue.edu>
2024-11-03 23:36:24 -0500

    made authors not plural

--- test/CreateNewUserTest.java
+++ test/CreateNewUserTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * remove friends. The system also supports account and relationship management for an interactive user experience.
  *
  * @version November 3, 2024
- * @authors Nick Wu, Chris Brantley, Ramya Prasanna, and Divya Vemireddy
+ * @author Nick Wu, Chris Brantley, Ramya Prasanna, and Divya Vemireddy
  */
 class CreateNewUserTest {
 
--- test/NewsPostTest.java
+++ test/NewsPostTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
  * remove friends. The system also supports account and relationship management for an interactive user experience.
  *
  * @version November 3, 2024
- * @authors Nick Wu, Chris Brantley, Ramya Prasanna, and Divya Vemireddy
+ * @author Nick Wu, Chris Brantley, Ramya Prasanna, and Divya Vemireddy
  */
 
 public class NewsPostTest {
