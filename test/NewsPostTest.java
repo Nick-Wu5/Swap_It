ed9611a68fc9409d3c9b5ecaceb1a7c2f4b25624
Ramya Prasanna <prasannr@purdue.edu>
2024-11-03 23:05:44 -0500

    Final fix of the test case NewsPostTest.java

--- test/NewsPostTest.java
+++ test/NewsPostTest.java
@@ -35,28 +35,4 @@ public class NewsPostTest {
         post.incrementDownvotes();
         assertEquals("The upvotes should equal 1 after being decremented" + post.getUpvotes(), "1");
     }
-    
-    public void testComments() {
-        // Assuming UserProfile and NewsComment classes are already defined
-        UserProfile author = new UserProfile("taylorswift246", "taylor.swift@gmail.com", "7685958484");
-
-        // Create a NewsPost instance
-        NewsPost post = new NewsPost(author, "Test Post Title", "/path/to/image.jpg", "2024-11-03");
-        
-        // Test adding comments
-        UserProfile commenter = new UserProfile("ryangosling", "ryan.gosling@gmail.com", "emmastone");
-        NewsComment comment1 = new NewsComment("Great post!", commenter);
-        NewsComment comment2 = new NewsComment("Thanks for sharing!", commenter);
-
-        post.addComment(comment1);
-        post.addComment(comment2);
-        comment1.incrementUpvotes();
-        comment1.incrementDownvotes();
-
-        // Verify comments were added
-        assertEquals("The number of comments made should equal 2" + post.getComments().size(), "2");
-        assertEquals("The author of the comment should match" + comment1.getAuthor().getUsername(), "ryangosling");
-        assertEquals("The contents of the comment should match" + comment2.getContent(), "Thanks for sharing!");
-        assertEquals("The UpVotes should equal 0" + comment1.getUpvotes(), "0");
-    }
 }
